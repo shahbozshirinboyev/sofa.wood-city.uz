86a15aa091a8780271945df179b4af13a9a5d3e1
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-11-26 12:15:15 +0500

    changing new HomePage

---
+++ src/components/slider/CustomSlider.jsx
@@ -0,0 +1,190 @@
+import { useState, useEffect } from "react";
+import "./CustomSlider.css";
+
+function CustomSlider({ children }) {
+  const [activeIndex, setActiveIndex] = useState(-1);
+  const [slideDone, setSlideDone] = useState(true);
+  const [timeID, setTimeID] = useState(null);
+
+  useEffect(() => {
+    setActiveIndex(0);
+  }, []);
+
+  useEffect(() => {
+    if (slideDone) {
+      setSlideDone(false);
+      setTimeID(
+        setTimeout(() => {
+          slideNext();
+          setSlideDone(true);
+        }, 5000)
+      );
+    }
+  }, [slideDone]);
+
+  const slideNext = () => {
+    setActiveIndex((val) => {
+      if (val >= children.length - 1) {
+        return 0;
+      } else {
+        return val + 1;
+      }
+    });
+  };
+
+  const slidePrev = () => {
+    setActiveIndex((val) => {
+      if (val <= 0) {
+        return children.length - 1;
+      } else {
+        return val - 1;
+      }
+    });
+  };
+
+  const AutoPlayStop = () => {
+    if (timeID > 0) {
+      clearTimeout(timeID);
+      setSlideDone(false);
+    }
+  };
+
+  const AutoPlayStart = () => {
+    if (!slideDone) {
+      setSlideDone(true);
+    }
+  };
+  //   ==================================================================
+
+  //   const [activeBox, setActiveBox] = useState(-1);
+
+  //   useEffect(() => {
+  //     setActiveBox(0);
+
+  //     const interval = setInterval(() => {
+  //       setActiveBox((prev) => (prev + 1) % 4); // 3 ta box uchun
+  //     }, 5000);
+  //     return () => clearInterval(interval);
+  //   }, []);
+
+  return (
+    <>
+      <div
+        className="container__slider"
+        // onMouseEnter={AutoPlayStop}
+        // onMouseLeave={AutoPlayStart}
+      >
+        {children.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className={
+                "slider__item slider__item-active-" + (activeIndex + 1)
+              }
+            >
+              {item}
+            </div>
+          );
+        })}
+
+        {/* <div className="container__slider__links">
+          {children.map((item, index) => {
+            return (
+              <button
+                key={index}
+                className={
+                  activeIndex === index
+                    ? "container__slider__links-small container__slider__links-small-active"
+                    : "container__slider__links-small"
+                }
+                onClick={(e) => {
+                  e.preventDefault();
+                  setActiveIndex(index);
+                }}
+              ></button>
+            );
+          })}
+        </div> */}
+
+        {/* <button
+          className="slider__btn-next"
+          onClick={(e) => {
+            e.preventDefault();
+            slideNext();
+          }}
+        >
+          {">"}
+        </button> */}
+
+        {/* <button
+          className="slider__btn-prev"
+          onClick={(e) => {
+            e.preventDefault();
+            slidePrev();
+          }}
+        >
+          {"<"}
+        </button> */}
+      </div>
+
+      <div className="flex justify-center items-center">
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-0">
+          {[
+            {
+              name: "Самый большой каталог мебели",
+              icon: "bi-compass",
+            },
+            {
+              name: "Найдется вся мебель",
+              icon: "bi-search",
+            },
+            {
+              name: "Оценка изделий независимыми экспертами",
+              icon: "bi-bar-chart",
+            },
+            {
+              name: "Реальные отзывы из популярных соцсетей",
+              icon: "bi-chat-dots",
+            },
+          ].map((item, index) => (
+            <div
+              key={index}
+              className={`relative transition-all duration-300 text-maincolor ${
+                activeIndex === index ? "bg-maincolor bg-opacity-10" : ""
+              }`}
+            >
+              <div
+                className={`flex cursor-pointer`}
+                onClick={() => {
+                  setActiveIndex(index);
+                }}
+              >
+                <div className="flex justify-center items-center p-4">
+                  <i className={`bi ${item.icon} text-[24px] md:text-[35px]`}></i>
+                </div>
+                <div className="w-[220px] flex justify-start items-center">
+                  <p className="text-[13px] md:text-[16px]">
+                    {" "}{item.name}{" "}
+                  </p>
+                </div>
+              </div>
+              <div
+                className={`${
+                  activeIndex === index ? "opacity-100" : "opacity-0"
+                }`}
+              >
+                <div
+                  className={`absolute bottom-0 left-0 h-[4px] bg-maincolor transition-all duration-[5000ms] ${
+                    activeIndex === index ? "w-full" : "w-0"
+                  }`}
+                ></div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CustomSlider;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,60 +1,101 @@
-import React, { useState, useEffect } from "react";
-import sliderImg1 from "../../public/home_slider/slider-1.jpg";
-import sliderImg2 from "../../public/home_slider/slider-2.jpg";
-import sliderImg3 from "../../public/home_slider/slider-3.jpg";
+import CustomSlider from "../components/slider/CustomSlider";
+import { useState, useEffect } from "react";
 
 function HomePage() {
-  const [activeBox, setActiveBox] = useState(-1);
-
-  useEffect(() => {
-    // Dastlab birinchi box uchun animatsiyani boshlash
-    setActiveBox(0);
-
-    const interval = setInterval(() => {
-      setActiveBox((prev) => (prev + 1) % 4); // 3 ta box uchun
-    }, 5000); // Har 5 soniyada almashadi
-
-    return () => clearInterval(interval);
-  }, []);
+  const images = [
+    {
+      imgURL: "../../public/home_slider/slider-1.jpg",
+      imgAlt: "Slider IMG 1",
+    },
+    {
+      imgURL: "../../public/home_slider/slider-2.jpg",
+      imgAlt: "Slider IMG 2",
+    },
+    {
+      imgURL: "../../public/home_slider/slider-3.jpg",
+      imgAlt: "Slider IMG 3",
+    },
+    {
+      imgURL: "../../public/home_slider/slider-4.jpg",
+      imgAlt: "Slider IMG 4",
+    },
+  ];
 
   return (
     <>
-      <div
-        className="relative w-full h-[500px] bg-cover bg-center"
-        style={{ backgroundImage: `url(${sliderImg1})` }}
-      >
-        <div className="absolute top-[100%] md:top-[82%] lg:top-[87%] w-full flex justify-center">
-          <div className="grid grid-cols-2 lg:grid-cols-4 gap-0 border">
-            {[0, 1, 2, 3].map((boxIndex) => (
-              <div
-                key={boxIndex}
-                className={`relative w-auto h-auto transition-opacity duration-500`}
-              >
-                <div className="flex bg-white border border-gray-100">
-                  <div className="flex justify-center items-center p-4">
-                    <i className="bi bi-compass text-[24px] md:text-[35px]"></i>
-                  </div>
-                  <div className="w-[180px] flex justify-center items-center">
-                    <p className="text-[13px] md:text-[16px]">
-                      {" "}Самый большой каталог мебели{" "}
-                    </p>
-                  </div>
-                </div>
-                <div
-                  className={`${
-                    activeBox === boxIndex ? "opacity-100" : "opacity-0"
-                  }`}
-                >
-                  <div
-                    className={`absolute bottom-0 left-0 h-[4px] bg-maincolor transition-all duration-[5000ms] ${
-                      activeBox === boxIndex ? "w-full" : "w-0"
-                    }`}
-                  ></div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+      <CustomSlider>
+        {images.map((image, index) => {
+          return (
+            <img
+              key={index}
+              src={image.imgURL}
+              alt={image.imgAlt}
+              className="h-[280px] md:h-[400px] lg:h-[500px] object-cover"
+            />
+          );
+        })}
+      </CustomSlider>
+
+      <div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
+          veniam, consectetur dolores magnam odit, fugit maiores praesentium
+          minima quidem minus deserunt tempora cupiditate assumenda voluptas
+          vero error nulla corrupti molestias ab tenetur voluptate quis
+          provident unde aperiam. Explicabo laborum deleniti corrupti et omnis
+          itaque pariatur. Sequi facere laborum est nisi dolore provident
+          sapiente laboriosam iste aspernatur itaque? Numquam id eligendi culpa
+          ducimus? Consequatur aspernatur animi quisquam impedit. Accusamus
+          laboriosam consequatur iusto eligendi fugiat possimus! Fugiat,
+          expedita. Impedit blanditiis repellat culpa? Hic illo perferendis
+          libero quidem consectetur delectus recusandae quisquam cum beatae
+          incidunt impedit suscipit, quia at nisi sed labore voluptatem numquam
+          et odit maxime placeat eaque expedita iusto. Corrupti, modi velit!
+          Delectus eum possimus autem quasi expedita animi officia totam labore
+          nihil dolor dolores culpa itaque facere consequuntur ea, officiis
+          reprehenderit dolorum blanditiis dolorem! Consequuntur tempora
+          deleniti eius magnam omnis tenetur soluta aliquid mollitia dignissimos
+          vitae cum accusamus, nobis incidunt ratione ducimus minima minus
+          neque. Maiores similique corrupti debitis, ullam eaque saepe sapiente
+          eligendi eos tempora quisquam quas, obcaecati alias facilis, harum
+          itaque repudiandae odio mollitia id laudantium recusandae quaerat
+          nihil soluta. Ducimus voluptatum maiores non dignissimos corrupti
+          natus, reprehenderit laboriosam similique architecto totam id
+          consectetur. Eaque porro quas ullam maiores voluptatum. Est dolore
+          nobis odio deserunt fugiat veritatis nihil placeat quibusdam, sit enim
+          porro suscipit, laudantium, vel atque quae autem quam amet? Aut cumque
+          laboriosam repellendus omnis perferendis deleniti minima aperiam
+          voluptatum corrupti vitae illo, corporis quia dolorum magnam optio
+          explicabo. Beatae reprehenderit optio dignissimos rem. Incidunt
+          repudiandae minus harum fuga? Quod doloremque deserunt temporibus
+          laudantium explicabo. Explicabo laborum sint atque, culpa minima sit
+          ut suscipit eum neque ipsam non eveniet architecto, expedita labore
+          veritatis, officia consequatur rem doloribus numquam nam. In,
+          accusantium nisi! Optio labore, sit sed doloribus possimus reiciendis
+          explicabo error fugiat veniam, laborum tempora sequi quia? Ratione
+          consequuntur accusantium ea laudantium cumque porro aperiam vitae
+          dignissimos at. Autem, ducimus animi corporis ratione, dolore dicta
+          harum reprehenderit, itaque delectus provident maiores. Alias corporis
+          doloremque voluptas quod optio quibusdam deleniti non voluptatum,
+          debitis saepe rerum assumenda sequi tenetur officia molestiae
+          repudiandae cupiditate architecto accusantium culpa unde eos?
+          Repellendus ut nostrum deserunt nam voluptate, sint magnam dolores
+          quae tempora ea aspernatur provident eaque illo ducimus vero, odio
+          quod sequi? Laborum dolore ratione corrupti neque quasi mollitia
+          expedita voluptatem exercitationem odit rem rerum repellendus quidem
+          facere sapiente, iure itaque fugit fuga. Rem id impedit alias. Error,
+          ab. Maiores, eos quidem. Magni quis voluptatem non dicta quidem error
+          porro facere modi veniam animi nulla, reiciendis, ullam hic. Nihil
+          tempore officia qui itaque eligendi sunt tenetur voluptate nemo, unde
+          delectus rem sed sint officiis id aliquam quam debitis expedita
+          suscipit, facilis perferendis optio cumque in quas et. Excepturi iste
+          modi nisi et iure eius perspiciatis dolorem nobis aut beatae
+          doloremque officiis tempora nostrum numquam minus quibusdam ipsam esse
+          ad, maiores vero in error, obcaecati inventore! Corrupti sit autem
+          nihil rerum, natus optio molestias eaque delectus cumque odio, saepe
+          architecto exercitationem ut facilis minus. Doloribus voluptatibus
+          illo architecto id, quidem molestias quae iste!
+        </p>
       </div>
     </>
   );
