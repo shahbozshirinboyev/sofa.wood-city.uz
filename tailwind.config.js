ca93c9c8c674a4efb40b3035a1a9f4de492a4f57
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-11-28 10:16:25 +0500

    Add Daisy UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,15 @@ export default {
         center: true,
         padding: {
           DEFAULT: "0.5rem",
-          sm: "0.5rem",
-          lg: "0.7rem",
-          xl: "1.5rem",
+          "sm ": "0.5rem",
+          "lg ": "0.7rem",
+          "xl ": "1.5rem",
           "2xl": "1.5rem",
         },
       },
     },
     
   },
-  plugins: [],
+  plugins: [require('daisyui'),],
+  daisyui: { darkTheme: false, },
 };
