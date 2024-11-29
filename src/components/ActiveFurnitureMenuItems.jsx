8dbb8117d5f03fd4d0683b4f026cb43d99d2ffc5
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-11-29 17:43:54 +0500

    new button flex-gow-1

--- src/components/ActiveFurnitureMenuItems.jsx
+++ src/components/ActiveFurnitureMenuItems.jsx
@@ -24,6 +24,7 @@ function ActiveFurnitureMenuItems() {
           {activeMenuFurniture?.items.map((product) => (
             <div className="border py-2 px-4">
               <img src={product.image0} alt="" className="h-[220px] mx-auto" />
+
               <div className="flex gap-4">
                 <p className="text-start line-clamp-2 font-semibold w-full">{product.name}</p>
                 <div className="text-end">
@@ -31,10 +32,28 @@ function ActiveFurnitureMenuItems() {
                   <p className="whitespace-nowrap line-through text-[14px] font-semibold opacity-50">{product.fixprice}</p>
                 </div>
               </div>
+
+              <div className="pt-4 flex gap-4">
+                <button className="btn btn-sm flex-1"><i className="bi bi-arrow-up-right"></i> Подробнее </button>
+                <button className="btn btn-sm flex-1"><i class="bi bi-cart"></i> В корзину </button>
+              </div>
+
+              <div className="py-4">
+                <p className="font-semibold">Габариты:</p>
+                <div className="grid grid-cols-3 text-start text-[14px] gap-2 py-1">
+                  <div> <p className="opacity-60">Длина</p> <p>189 sm</p> </div>
+                  <div> <p className="opacity-60">Ширина</p> <p>103 sm</p> </div>
+                  <div> <p className="opacity-60">Высота</p> <p>90 sm</p> </div>
+                </div>
+              </div>
+
+              {/* 
               <p>{product.status}</p>
               <p>
                 {product.bestseller === true ? "бестселлер" : "не бестселлер"}
-              </p>
+              </p> 
+              */}
+
             </div>
           ))}
         </div>
